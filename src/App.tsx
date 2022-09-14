bd1d348fc1e6bee532b4d58e63c861020a7967a9
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-09-13 22:30:10 -0300

    feature: Adiciona Title

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="bg-nlw-gradient text-transparent bg-clip-text ">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
     </div>
   );
 }
